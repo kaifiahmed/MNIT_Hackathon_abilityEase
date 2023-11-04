ababefce7b4a9df6dbe5236b8de90972f1fc4769
vidisha21 <vidishamamnani921@gmail.com>
2023-11-04 18:48:32 +0530

    navigation

--- src/components/navigationBar/index.jsx
+++ src/components/navigationBar/index.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import Wrapper from './style'
 
-const Navigation
- = () => {
+const NavigationBar = () => {
   return (
-    <div>Navigation
-        
+    <Wrapper>
+      <div className="navbar">
+      <div className="logo">
+        <img src= {logo} alt="Logo" />
+      </div>
+      <div className="nav-items">
+        <a href="#">Home</a>
+        <a href="#">About Us</a>
+        <a href="#">Services</a>
+        <a href="#">Support</a>
+      </div>
     </div>
+    </Wrapper>
+    
   )
 }
 
-export default Navigation
+export default NavigationBar
