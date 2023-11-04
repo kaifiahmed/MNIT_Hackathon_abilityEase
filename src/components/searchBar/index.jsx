19b73a120e09ae00bd3f4a4be1a77263fe35354d
Neha Meerchandani <nehameerchandani1303@gmail.com>
2023-11-04 06:30:07 -0700

    serch

--- src/components/searchBar/index.jsx
+++ src/components/searchBar/index.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import searchbutton from '../../assets/images/searchbutton.png'
+import Wrapper from './style'
 
 const SearchBar
  = () => {
   return (
-    <div>
+    <Wrapper>
     <input
    type="text"
    placeholder="Enter your search query"
    />
    <input type="button" className='button'/>
    <img src={searchbutton}  alt='image' height='50px'/>
-</div>
+</Wrapper>
   )
 }
 
---
+++ src/components/searchBar/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+input[type = text ]{
+    margin : 10px
+}
+`
+export default Wrapper;
