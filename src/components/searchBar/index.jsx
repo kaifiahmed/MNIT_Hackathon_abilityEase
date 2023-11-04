af8687a8ff091a15e73528b2854713026f1d0d6f
Neha Meerchandani <nehameerchandani1303@gmail.com>
2023-11-04 06:17:19 -0700

    search

--- src/components/searchBar/index.jsx
+++ src/components/searchBar/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import searchbutton from '../../assets/images/searchbutton.png'
 
 const SearchBar
  = () => {
   return (
-    <div>SearchBar
-        
-    </div>
+    <div>
+    <input
+   type="text"
+   placeholder="Enter your search query"
+   />
+   <input type="button" className='button'/>
+   <img src={searchbutton}  alt='image' height='50px'/>
+</div>
   )
 }
 
