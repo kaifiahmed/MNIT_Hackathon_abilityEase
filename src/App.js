b32337f7264d2b816249a68747c24f1b6beeda66
vidisha21 <vidishamamnani921@gmail.com>
2023-11-05 12:31:31 +0530

    support

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Footer from './components/footer'
 import Newresult from './components/newresult';
 import AboutUs from './components/aboutus';
 import SupportPage from './components/supportPage';
+import Services from './components/services';
 
 
 const App = () => {
@@ -23,11 +24,11 @@ const App = () => {
       {/* <VoiceSearch /> */}
       <Routes>
         <Route path='/' element={<><SearchBar/> <Features /></>} />
-        <Route path="/dairy" element={<DairyNote/>} /> {/* 👈 Rfasfenders at /app/ */}
+        <Route path="/dairy" element={<DairyNote/>} /> 
         <Route path="/news-results" element={<Newresult/>} />
         <Route path="/aboutus" element={<AboutUs/>} />
-        <Route path="/support" element={<SupportPage/>} />  {/* 👈 Rfasfenders at /app/ */}
-
+        <Route path="/support" element={<SupportPage/>} />  
+        <Route path="/services" element={<Services/>} /> 
       </Routes>
       <Footer/>
     </BrowserRouter>
