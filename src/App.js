c338475007f2f3f9e7f513472434b06a36f2c4c6
vidisha21 <vidishamamnani921@gmail.com>
2023-11-05 09:14:49 +0530

    fix news

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react'
-import NavigationBar from './components/navigationBar'
-import SearchBar from './components/searchBar'
-import Feactures from './components/features'
-import Features from './components/features'
-import VoiceSearch from './components/voiceSearch'
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import DairyNote from './components/dairyNote'
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavigationBar from './components/navigationBar';
+import Features from './components/features';
+import SearchBar from './components/searchBar';
+import Newresult from './components/newresult';
+import AboutUs from './components/aboutus';
 
 const App = () => {
   return (
     <Router>
-    <Routes>
-      <Route path="/" element={<Features />} />
-      <Route path="/news-results" element={<Newresult />} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <NavigationBar />
+              <SearchBar />
+          
+              <Features />
+              </>
+          }
+        />
+        <Route path="/news-results" element={<Newresult/>} />
+     
+
       <Route path="/navigation" element={<NavigationBar />} />
       <Route path="/searchbar" element={<SearchBar />} />
       <Route path="/features" element={<Features />} />
+      <Route path="/aboutus" element={<AboutUs />} />
+
       
     </Routes>
   </Router>
