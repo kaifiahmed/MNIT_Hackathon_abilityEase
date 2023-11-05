e6e4d200c8a611881086a6c5aab3b1dd568d3d88
vidisha21 <vidishamamnani921@gmail.com>
2023-11-05 10:12:03 +0530

    aboutus

--- src/components/aboutus/style.js
+++ src/components/aboutus/style.js
@@ -5,6 +5,10 @@ max-width: 800px;
   margin: 0 auto;
   padding: 20px;
   text-align: center;
+  background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3mtOMjg4agAVf1-yeNJtpaCoUNQTCELXFsQ&usqp=CAUgit'); 
+  background-size: cover;
+  background-position: center;
+  
 
   h1 {
     font-size: 2em;
