2e6654dc41c301553fdc685fd58dd63172c0a2e5
vidisha21 <vidishamamnani921@gmail.com>
2023-11-05 12:32:42 +0530

    support

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Footer from './components/footer'
 import Newresult from './components/newresult';
 import AboutUs from './components/aboutus';
 import SupportPage from './components/supportPage';
-import Services from './components/services';
 
 
 const App = () => {
@@ -28,7 +27,6 @@ const App = () => {
         <Route path="/news-results" element={<Newresult/>} />
         <Route path="/aboutus" element={<AboutUs/>} />
         <Route path="/support" element={<SupportPage/>} />  
-        <Route path="/services" element={<Services/>} /> 
       </Routes>
       <Footer/>
     </BrowserRouter>
